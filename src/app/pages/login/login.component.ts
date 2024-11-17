70bdb9a913b0cba9a3654fd2617e8d63bcf4bd4d
Motiar Rahaman <rahamanmotiar70@gmail.com>
2024-11-18 04:41:49 +0530

    auth_api

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,23 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, HttpClientModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginObj: Login;  // Object to hold form data
   form: FormGroup;
-  constructor(private fb: FormBuilder) {
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.loginObj = new Login();
     this.form = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required])
-    })
+    });
   }
-onSubmit(){
-  if(this.form.valid){
-    console.log('success',this.form.value)
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.loginObj.EmailId = this.form.value.email;
+      this.loginObj.Password = this.form.value.password;
+
+      console.log('Submitting login:', this.loginObj);
+
+      // Make API call
+      this.http.post('https://freeapi.miniprojectideas.com/api/User/Login', this.loginObj).subscribe(
+        (res: any) => {
+          if (res.result) {
+            alert('Login Success');
+          } else {
+            alert(res.message || 'Something went wrong');
+          }
+        },
+        (error: any) => {
+          console.error('Error occurred:', error);
+          alert('An error occurred. Please try again.');
+        }
+      );
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
+
+export class Login {
+  EmailId: string;
+  Password: string;
+
+  constructor() {
+    this.EmailId = '';
+    this.Password = '';
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,25 +1,45 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, HttpClientModule,CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-form:FormGroup;
-constructor(private fb:FormBuilder){
-this.form=this.fb.group({
-  name:new FormControl('',[Validators.required]),
-  email:new FormControl('',[Validators.required,Validators.email]),
-  password:new FormControl('',[Validators.required]),
-})
-}
-onSubmit(){
-  if(this.form.valid){
-    console.log(this.form.value)
+  form: FormGroup;
+  
+  // Inject HttpClient in the constructor
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
-}
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formData = this.form.value;
+
+      // Make a POST request to the API
+      this.http.post('https://freeapi.miniprojectideas.com/api/User/CreateNewUser', formData)
+        .subscribe(
+          (response) => {
+            console.log('User registered successfully:', response);
+            alert('User registered successfully!');
+          },
+          (error) => {
+            console.error('Error registering user:', error);
+            alert('Registration failed. Please try again.');
+          }
+        );
+    } else {
+      alert('Please fill out the form correctly.');
+    }
+  }
 }
